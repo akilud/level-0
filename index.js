e9ce70c1e4354070febaf24b9ccbb017fd93637f
akil <akilud@gmail.com>
2017-08-20 14:19:40 +0530

    corrected handle

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  var json = {"handle":"akil"}
+  var json = {"handle":"akilud"}
   res.send(json)
 })
 
